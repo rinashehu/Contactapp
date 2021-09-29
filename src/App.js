1bf5fef3059c427df70d31eb6f411f53831682b5
Rina Shehu <rina.shehu@studentet.uni-pr.edu>
2021-09-29 20:52:40 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { uuid } from "uuidv4";
+import "./App.css";
+import Header from "./components/Header";
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const AddContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <Router>
+        <Header />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={() => (
+              <ContactList
+                contacts={contacts}
+                getContactId={removeContactHandler}
+              />
+            )}
+          />
+          <Route
+            path="/add"
+            component={() => (
+              <AddContact AddContactHandler={AddContactHandler} />
+            )}
+          />
+        </Switch>
+        {/* <AddContact AddContactHandler={AddContactHandler} /> */}
+        {/* <ContactList contacts={contacts} getContactID={removeContactHandler} /> */}
+      </Router>
     </div>
   );
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ContactCard from "./ContactCard";
+
+const ContactList = (props) => {
+  console.log(props);
+  const deleteConactHandler = (id) => {
+    props.getContactId(id);
+  };
+
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <ContactCard
+        contact={contact}
+        clickHander={deleteConactHandler}
+        key={contact.id}
+      ></ContactCard>
+    );
+  });
+  return (
+    <div class="main">
+      <h2>Contact List</h2>
+      <Link to="/add">
+        <button className="ui button blue right">Add Contact</button>
+      </Link>
+      <div className="ui celled list">{renderContactList}</div>
+    </div>
+  );
+};
+
+export default ContactList;
